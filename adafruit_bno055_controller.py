1d459b3211f6421c1dddd4f806184266efbbe0c2
ToshiNier <dog.album.koala@gmail.com>
2018-06-13 15:20:10 +0900

    mouakann

--- adafruit_bno055_controller.py
+++ adafruit_bno055_controller.py
@@ -15,11 +15,11 @@ class AdafruitBno055Controller:
             logging.basicConfig(level=logging.DEBUG)
 
         # Initialize the BNO055 and stop if something went wrong.
-        if not bno.begin():
+        if not self.bno.begin():
             raise RuntimeError('Failed to initialize BNO055! Is the sensor connected?')
 
         # Print system status and self test result.
-        status, self_test, error = bno.get_system_status()
+        status, self_test, error = self.bno.get_system_status()
         print('System status: {0}'.format(status))
         print('Self test result (0x0F is normal): 0x{0:02X}'.format(self_test))
         # Print out an error if system status is in error mode.
@@ -28,7 +28,7 @@ class AdafruitBno055Controller:
             print('See datasheet section 4.3.59 for the meaning.')
 
         # Print BNO055 software revision and other diagnostic data.
-        sw, bl, accel, mag, gyro = bno.get_revision()
+        sw, bl, accel, mag, gyro = self.bno.get_revision()
         print('Software version:   {0}'.format(sw))
         print('Bootloader version: {0}'.format(bl))
         print('Accelerometer ID:   0x{0:02X}'.format(accel))
