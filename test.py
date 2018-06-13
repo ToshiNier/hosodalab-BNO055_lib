60926b494f538317eca6f3e7a2deca522f97b00e
ToshiNier <dog.album.koala@gmail.com>
2018-06-13 15:21:30 +0900

    dekita

--- test.py
+++ test.py
@@ -1,11 +1,12 @@
 #!/usr/bin/python
 
-from adafruit_bno055_controller import AdafruitBno055Controller
-from xl_config import XLConfig
+from adafruit_bno055_controller import *
+from xl_config import *
 
 def main():
     bno_controller = AdafruitBno055Controller()
     x,y,z = bno_controller.gravity()
+    print x,y,z
     
 
 if __name__ == "__main__":
